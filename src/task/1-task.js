134f066d9db877f33aa52d6b9e39bd50a3ed9543
BreakthroughGamer <55695819+BreakthroughGamer@users.noreply.github.com>
2024-12-16 14:28:47 +0530

    fixes

--- src/task/1-task.js
+++ src/task/1-task.js
@@ -1,12 +1,29 @@
 import { namespaceWrapper } from "@_koii/namespace-wrapper";
+import express from "express";
+import path from "path";
 
 export async function task(roundNumber) {
   // Run your task and store the proofs to be submitted for auditing
   // The submission of the proofs is done in the submission function
   try {
     console.log(`EXECUTE TASK FOR ROUND ${roundNumber}`);
-    // you can optionally return this value to be used in debugging
-    await namespaceWrapper.storeSet("value", "Hello, World!");
+
+    // const app = express();
+    // const PORT = 3000;
+
+    // // Serve static files
+    // app.use(express.static(path.resolve(".")));
+
+    // Default route
+    // app.get("/", (req, res) => {
+    //   res.sendFile(path.resolve("index.html"));
+    // });
+
+    // // Start the server
+    // app.listen(PORT, () => {
+    //   console.log(`Server is running at http://localhost:${PORT}`);
+    // });
+
   } catch (error) {
     console.error("EXECUTE TASK ERROR:", error);
   }
